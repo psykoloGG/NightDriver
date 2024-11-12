f7b32eecd1ae1eb7d9ea583ee2e240e14261178b
Lukas Kundelis <lukas.kundelis@gmail.com>
2024-11-12 04:00:23 +0000

    Renamed project, added beatmapper plugin, moved prototypes

---
+++ Source/NightDriver/Private/NightDriverGamemode.cpp
@@ -0,0 +1,2 @@
+#include "NightDriverGamemode.h"
+
---
+++ Source/NightDriver/Public/NightDriverGamemode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NightDriverGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTDRIVER_API ANightDriverGamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
